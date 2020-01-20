3d7aefdc91acb30ffc63e85d68e38c041d7b36e9
Nycroll <60111348+Nycroll@users.noreply.github.com>
2020-01-20 19:57:42 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const token = 'NjY4NDgxOTUzMzk3MjExMTk3.XiXcdg.g4Nmr515a7U-G0eQ_chxkb8ct40'
+const prefix = '?'
+usedCommandRecently4 = new Set();
+
+client.on('ready', () => {
+    console.log('gen bot is now online')
+    
+    client.user.setActivity("Invite", { type: "invite.gg/nycrollcracks" });
+});
+client.on('message', message =>{
+    if (message.content === 'hello'){
+        message.author.send('hi');
+    };
+});
+client.on('message', message =>{
+    if (!message.guild) return;
+if (message.content === '?Spotify'){
+    if (usedCommandRecently4.has(message.author.id)){
+        message.channel.send('Your **Cooldown** expires in one hour.')
+    } else{
+        usedCommandRecently4.add(message.author.id);
+        setTimeout(() =>{
+            usedCommandRecently4.delete(message.author.id);
+        }, 36000000 )
+    var string = `spotify1
+    spotify2
+    spotify3
+    spotify4
+    spotify5`
+    var words = string.split('\n');
+    let random = words[Math.floor(Math.random()*words.length)];
+    message.author.send(`${random}`);
+};
+};
+});
+client.login(token);
